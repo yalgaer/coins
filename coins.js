ad19895e6bb3df487219edc1fbca866fc5ded64b
Yuliya Algaer <yalgaer@gmail.com>
2017-05-12 09:19:07 +0700

    New collections adding

--- coins.js
+++ coins.js
@@ -31,15 +31,21 @@ function updateCoins()
     $.get("get-coins.php", function (data) {
         var html = "";
         html += "<table>";
-        html += "<tr><th>Монеты</th><th>Год</th><th>Фото</th></tr>";
+        html += "<tr><th>№</th><th>Наименование</th><th>Год</th><th>Двор</th><th>Фото</th></tr>";
         for(var i = 0; i < data.length; i++){
             var coins = data[i];
             html += "<tr>";
+            html += "<td>";
+            html += coins.pos;
+            html += "</td>";
             html += "<td>";
             html += coins.name;
             html += "</td>";
             html += "<td>";
             html += coins.year;
+            html += "</td>";
+            html += "<td>";
+            html += coins.mint;
             html += "</td>";
             html += "<td>";
             html += "<img src='"+coins.img+"'/>";
